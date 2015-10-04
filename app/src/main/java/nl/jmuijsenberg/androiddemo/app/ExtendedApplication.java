3dec6ae07c6dd256a62c5a2d66ee40a48c4cbfa9
jvdmuijse <johan.van.den.muijsenberg@gmail.com>
2015-10-04 22:40:34 +0200

    Add custom application class

---
+++ app/src/main/java/nl/jmuijsenberg/androiddemo/app/ExtendedApplication.java
@@ -0,0 +1,11 @@
+package nl.jmuijsenberg.androiddemo.app;
+
+import android.app.Application;
+
+public class ExtendedApplication extends Application {
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+}
