2aafdecf8a4d70d3d4f829b2ef8fff13ae95f0c9
jvdmuijse <johan.van.den.muijsenberg@gmail.com>
2015-10-05 20:36:46 +0200

    Integrate Facebook Stetho

--- app/src/main/java/nl/jmuijsenberg/androiddemo/app/ExtendedApplication.java
+++ app/src/main/java/nl/jmuijsenberg/androiddemo/app/ExtendedApplication.java
@@ -2,10 +2,24 @@ package nl.jmuijsenberg.androiddemo.app;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+
+import nl.jmuijsenberg.androiddemo.BuildConfig;
+
 public class ExtendedApplication extends Application {
     @Override
     public void onCreate()
     {
         super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Stetho.initialize(
+                    Stetho.newInitializerBuilder(this)
+                            .enableDumpapp(
+                                    Stetho.defaultDumperPluginsProvider(this))
+                            .enableWebKitInspector(
+                                    Stetho.defaultInspectorModulesProvider(this))
+                            .build());
+        }
     }
 }
