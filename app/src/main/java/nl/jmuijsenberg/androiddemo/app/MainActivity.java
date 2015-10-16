1da04023e4b63806338c8e56dae8229f559442bf
jvdmuijse <johan.van.den.muijsenberg@gmail.com>
2015-10-16 23:55:06 +0200

    Use floating action button

--- app/src/main/java/nl/jmuijsenberg/androiddemo/app/MainActivity.java
+++ app/src/main/java/nl/jmuijsenberg/androiddemo/app/MainActivity.java
@@ -2,13 +2,11 @@ package nl.jmuijsenberg.androiddemo.app;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ImageButton;
+import android.support.v7.widget.Toolbar;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -23,16 +21,20 @@ import nl.jmuijsenberg.androiddemo.devices.android.NativeDevice;
 import nl.jmuijsenberg.androiddemo.repository.sqlite.RepositorySqlite;
 import nl.jmuijsenberg.androiddemo.viewmodels.MainViewModel;
 
-public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener, Fragment1.OnFragmentInteractionListener, Fragment2.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity  implements ViewPager.OnPageChangeListener, Fragment1.OnFragmentInteractionListener, Fragment2.OnFragmentInteractionListener{
     public MainViewModel mMainViewModel;
+
+    @Bind(R.id.fab)
+    public FloatingActionButton mFloatingActionButton;
+
+    @Bind(R.id.toolbar)
+    public Toolbar mToolbar;
 
     @Bind(R.id.viewPager)
     public ViewPager mViewPager;
 
     @Bind(R.id.pagerTabStrip)
     public PagerTabStrip mPagerTabStrip;
-
-    private ActionBar mActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,17 +43,7 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
 
         ButterKnife.bind(this);
 
-        mActionBar = getSupportActionBar();
-        if( mActionBar != null) {
-            mActionBar.setLogo(R.drawable.ic_3d_rotation_24dp);
-            mActionBar.setDisplayOptions(ActionBar.DISPLAY_USE_LOGO |
-                                         ActionBar.DISPLAY_SHOW_HOME |
-                                         ActionBar.DISPLAY_HOME_AS_UP |
-                                         ActionBar.DISPLAY_SHOW_TITLE |
-                                         ActionBar.DISPLAY_SHOW_CUSTOM);
-        }
-
-        //setSupportActionBar(); When using new ToolBar class
+        setSupportActionBar(mToolbar);
 
         mPagerTabStrip.setTextSpacing(10);
         mPagerTabStrip.setDrawFullUnderline(false);
@@ -62,29 +54,12 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
         mMainViewModel.mMainController.mDevice = new NativeDevice();
 
         mViewPager.setAdapter(new PageAdapter(getSupportFragmentManager()));
-
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    @OnClick(R.id.fab)
+    public void updateDate(FloatingActionButton button) {
+        android.support.v4.app.DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
     @Override
@@ -105,11 +80,5 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
     @Override
     public void onFragmentInteraction(Uri uri) {
 
-    }
-
-    @OnClick(R.id.updateDateButton)
-    public void updateDate(ImageButton button) {
-        android.support.v4.app.DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
